72700beb0aef12a81ba9c3ca06a614ea915c76d6
Stuartw-Source <stuartpaulwebb@gmail.com>
2025-05-18 21:47:32 +0100

    Script update

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 ##install script for StuartW-Source KDE-Plasma6 Dotfiles##
 
-sudo pacman -S fastfetch ghostty opera spotify guitarix --noconfirm
+sudo pacman -S --needed base-devel
+
+sudo pacman -S --noconfirm git
+
+git clone https://aur.archlinux.org/paru.git
+cd ~/paru
+makepkg -si
+
+sudo pacman -Syu fastfetch ghostty opera spotify guitarix --noconfirm
+
+cd ~/KDEPLASMA6-DOTTTFILES
+
+cp -R com.github.prayag2.modernclock -t ~/.local/share/plasma/plasmoids/
+cp -R org.kde.latte.spacer -t ~/.local/share/plasma/plasmoids/
+cp -R org.kde.plasma.plasm6desktopindicator -t ~/.local/share/plasma/plasmoids/
 
