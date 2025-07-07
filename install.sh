60554c1cc72c53d40d4b01202613b69ec447f75b
Stuartw-Source <stuartpaulwebb@gmail.com>
2025-07-07 22:42:31 +0100

    updates

--- install.sh
+++ install.sh
@@ -1,18 +1,46 @@
 ##install script for StuartW-Source KDE-Plasma6 Dotfiles##
 
+#install base-devel which is required for AUR managment
 sudo pacman -S --needed base-devel
 
+#install git
 sudo pacman -S --noconfirm git
 
+#download and install paru community repo manager
 git clone https://aur.archlinux.org/paru.git
 cd ~/paru
 makepkg -si
 
+#application installs
 sudo pacman -Syu fastfetch ghostty opera spotify guitarix --noconfirm
 
-cd ~/KDEPLASMA6-DOTTTFILES
+#enable bluetooth to start on startup
+sudo bluetoothctl enable
 
-cp -R com.github.prayag2.modernclock -t ~/.local/share/plasma/plasmoids/
-cp -R org.kde.latte.spacer -t ~/.local/share/plasma/plasmoids/
-cp -R org.kde.plasma.plasm6desktopindicator -t ~/.local/share/plasma/plasmoids/
+#wallpaper copy
+cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Goku_1.jpeg ~/pictures/wallpaper/
+cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Vegeta_1.jpeg ~/pictures/wallpaper/
 
+#fastfetch config copy
+cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/cat.png ~/.config/fastfetch/
+cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/config.jsonc ~/.config/fastfetch/
+
+#ghostty config copy
+cp ~/KDEPLAMSA6-DOTFILES/Applications/ghostty/config ~/.config/ghostty/
+
+#starship config copy
+cp ~/KDEPLASMA6-DOTFILES/starship/starship.toml ~/.config/
+
+#KDE desktop set up copy
+cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasma-org.kde.plasma.desktop-appletsrc ~/.config/
+cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmashellrc ~/.config/
+cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmoids ~/.local/share/plasma
+
+
+
+
+
+
+
+
+sudo reboot
