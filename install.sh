bcc9a88ca626c0f59a4044fe748fa7a258cfb872
Stuartw-Source <stuartpaulwebb@gmail.com>
2025-07-08 22:49:59 +0100

    install script updates

--- install.sh
+++ install.sh
@@ -1,42 +1,43 @@
 ##install script for StuartW-Source KDE-Plasma6 Dotfiles##
 
 #install base-devel which is required for AUR managment
-sudo pacman -S --needed base-devel
+run_cmd "sudo pacman -S --needed base-devel"
 
 #install git
-sudo pacman -S --noconfirm git
+run_cmd "sudo pacman -S --noconfirm git"
 
 #download and install paru community repo manager
-git clone https://aur.archlinux.org/paru.git
-cd ~/paru
-makepkg -si
+run_cmd "git clone https://aur.archlinux.org/paru.git"
+run_cmd "cd ~/paru"
+run_cmd "makepkg -si --noconfirm"
 
 #application installs
-sudo pacman -Syu fastfetch ghostty opera spotify guitarix --noconfirm
+run_cmd "sudo pacman -Syu fastfetch ghostty opera spotify guitarix visual-studio-code-bin ttf-jetbrains-mono-nerd --noconfirm"
 
 #enable bluetooth to start on startup
-sudo bluetoothctl enable
+run_cmd "sudo bluetoothctl enable bluetooth"
 
 #wallpaper copy
-cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Goku_1.jpeg ~/pictures/wallpaper/
-cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Vegeta_1.jpeg ~/pictures/wallpaper/
+run_cmd "cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Goku_1.jpeg ~/pictures/wallpaper/"
+run_cmd "cp ~/KDEPLAMSA6-DOTFILES/wallpaper/Vegeta_1.jpeg ~/pictures/wallpaper/"
 
 #fastfetch config copy
-cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/cat.png ~/.config/fastfetch/
-cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/config.jsonc ~/.config/fastfetch/
+run_cmd "cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/cat.png ~/.config/fastfetch/"
+run_cmd "cp ~/KDEPLAMSA6-DOTFILES/Applications/fastfetch/config.jsonc ~/.config/fastfetch/"
 
 #ghostty config copy
-cp ~/KDEPLAMSA6-DOTFILES/Applications/ghostty/config ~/.config/ghostty/
+run_cmd "cp ~/KDEPLAMSA6-DOTFILES/Applications/ghostty/config ~/.config/ghostty/"
 
 #starship config copy
-cp ~/KDEPLASMA6-DOTFILES/starship/starship.toml ~/.config/
+run_cmd "cp ~/KDEPLASMA6-DOTFILES/starship/starship.toml ~/.config/"
 
 #KDE desktop set up copy
-cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasma-org.kde.plasma.desktop-appletsrc ~/.config/
-cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmashellrc ~/.config/
-cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmoids ~/.local/share/plasma
+run_cmd "cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasma-org.kde.plasma.desktop-appletsrc ~/.config/"
+run_cmd "cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmashellrc ~/.config/"
+run_cmd "cp ~/KDEPLASMA6-DOTFILES/KDE-desktop/plasmoids ~/.local/share/plasma"
 
-
+#install nvidia drivers
+run_cmd "sudo pacman -S nvidia-open nvidia-utils"
 
 
 
